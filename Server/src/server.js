b4680333c304ea1fd3c6cdf77b1d96da73738eb7
Fitsum Zerihun <fitsa0909@gmail.com>
2025-05-21 23:57:53 +0300

    initial comit

--- Server/src/server.js
+++ Server/src/server.js
@@ -47,7 +47,8 @@ app.post("/register", (req, res) => {
         res.json(employee);
       })
       .catch((err) => res.json(err));
-  });
+  }).catch(err => console.log(err);
+  );
 
   console.log(`Posted Successfully`);
 });
