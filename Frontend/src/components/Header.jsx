9294c08cc24f487962df1de9beced6f03cadb6fc
Fitsum Zerihun <fitsa0909@gmail.com>
2025-05-24 16:51:06 +0300

    Update Header.jsx

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <div className="flex w-[412px] lg:w-full h-auto py-4 px-4 justify-between items-center text-white-500 fixed z-1 container">
       <div className="w-[50px] lg:w-[130px] pl-2 lg:pl-4 flex items-center relative">
@@ -42,6 +44,38 @@ const Header = () => {
           <h1 className="hover:text-[#16fe01]">LOGIN</h1>
         </Link>
       </nav>
+
+      <div
+        className="flex flex-col text-center relative "
+        onClick={() => setToggle(!toggle)}
+      >
+        <img className="menu" alt="" srcset="" />
+        {toggle && (
+          <div className="w-fit py-2 px-3 flex flex-col gap-2 justify-center text-[7px] lg:text-[15px] font-bold bg-gray-800 absolute top-12 -right-4">
+            <Link to="/Home">
+              <h1 className="hover:text-[#16fe01]">HOME</h1>
+            </Link>
+            <Link to="/AboutUs">
+              <h1 className="hover:text-[#16fe01]">ABOUTUS</h1>
+            </Link>
+            <Link to="/Services">
+              <h1 className="hover:text-[#16fe01]">SERVICES</h1>
+            </Link>
+            <Link to="/Vehicles">
+              <h1 className="hover:text-[#16fe01]">VEHICLES</h1>
+            </Link>
+            <Link to="/Contact">
+              <h1 className="hover:text-[#16fe01]">CONTACT</h1>
+            </Link>
+            <Link to="/SignUp">
+              <h1 className="hover:text-[#16fe01]">SIGNUP</h1>
+            </Link>
+            <Link to="/Login">
+              <h1 className="hover:text-[#16fe01]">LOGIN</h1>
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
