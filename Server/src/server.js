33978f9ce2eb4c7005a416d4538e0ccc9af694a7
Fitsum Zerihun <fitsa0909@gmail.com>
2025-05-28 00:14:37 +0300

    Update server.js

--- Server/src/server.js
+++ Server/src/server.js
@@ -68,7 +68,7 @@ app.delete("/dashboard/:id", async (req, res) => {
       return res.status(404).json({ error: "User not found" });
     }
 
-    res.status(200).json({ message: "User deleted successfully" }); // ✅ THIS!
+    res.status(200).json({ message: "User deleted successfully" }); 
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal server error" });
