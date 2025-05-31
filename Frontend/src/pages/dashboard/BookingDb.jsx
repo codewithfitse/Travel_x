7031af36aec9a68b1398573d17d732ad76b5b107
Fitsum Zerihun <fitsa0909@gmail.com>
2025-05-31 18:14:59 +0300

    Update BookingDb.jsx

--- Frontend/src/pages/dashboard/BookingDb.jsx
+++ Frontend/src/pages/dashboard/BookingDb.jsx
@@ -25,7 +25,7 @@ export const BookingDb = () => {
   return (
     <>
       <section className="min-h-screen overflow-x-hidden">
-        <div className="w-full h-full flex bg-[#020817] text-white">
+        <div className="w-full h-Screen flex bg-[#020817] text-white">
           <SideBar />
           <div className="ml-14 flex flex-col flex-1">
             <Header />
