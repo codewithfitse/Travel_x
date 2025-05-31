9cddf4d7b3a5227104bf6acb635f64dfffb66674
Fitsum Zerihun <fitsa0909@gmail.com>
2025-05-31 12:35:18 +0300

    Update AdminDashboard.jsx

--- Frontend/src/pages/dashboard/AdminDashboard.jsx
+++ Frontend/src/pages/dashboard/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import { Header, SideBar } from "../dashboard/component";
 import axios from "axios";
 
