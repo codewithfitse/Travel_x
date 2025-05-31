82e95c960e79dc29222657369bbe75227f15c26c
Fitsum Zerihun <fitsa0909@gmail.com>
2025-05-31 20:57:30 +0300

    Update Landing.jsx

--- Frontend/src/pages/Landing.jsx
+++ Frontend/src/pages/Landing.jsx
@@ -4,12 +4,13 @@ import { Button } from "../components/Button";
 
 export const Landing = () => {
   return (
-    <div className="w-full h-[100%] landingBg overflow-y-hidden">
-      <div className="h-screen pt-130 lg:pt-72 pl-5 lg:pl-15 pb-32 lg:pb-9 backdrop-blur-[5px]">
-        <h1 className="mb-3 text-[25px]/7 text-white font-serif lg:text-6xl/15 font-bold">
-          <span className="text-[#16fe01]">Travel-X</span> <br />
-          Car Rental Website
+    <div className="w-full h-screen landingBg overflow-y-hidden">
+      <div className="h-screen pt-130 lg:pt-72 pl-5 lg:pl-15 pb-32 lg:pb-9 text-[25px]/7 text-white font-serif lg:text-6xl/15 font-bold backdrop-blur-[5px]">
+        <h1 className="mb-3 text-[#16fe01]">
+          <span className="text-[80px] font-serif">T</span>ime
+          <br />
         </h1>
+        <h1 className="">Car Rental Website</h1>
         <p className="text-white text-[15px] lg:text-[25px]/8 font-bold">
           Find a variety of the car of your dreams, <br />
           at a good price and quality
