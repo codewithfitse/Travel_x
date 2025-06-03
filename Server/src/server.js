e79a72c474f53224df522e2cd0256bb7bae3ab28
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-03 12:39:15 +0300

    Update server.js

--- Server/src/server.js
+++ Server/src/server.js
@@ -18,7 +18,7 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 const PORT = 3000;
 
 const MONGO_DB = "mongodb+srv://user:user123@cluster0.ooin5ux.mongodb.net/User";
