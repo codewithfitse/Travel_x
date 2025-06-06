676e582e28fb121b1ad8fbaa083dbbb987a4a477
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-06 20:35:00 +0300

    Update server.js

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path";
 import bcrypt from "bcryptjs";
 import multer from "multer";
 import UserLogin from "../models/usersDb.js";
