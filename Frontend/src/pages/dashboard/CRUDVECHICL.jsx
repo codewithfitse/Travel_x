b8054af0a4a6aa311c0deafc0e5cf570bd23aa4c
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-07 21:38:19 +0300

    Update CRUDVECHICL.jsx

--- Frontend/src/pages/dashboard/CRUDVECHICL.jsx
+++ Frontend/src/pages/dashboard/CRUDVECHICL.jsx
@@ -391,7 +391,7 @@ export const Edits = () => {
             <input
               type="file"
               accept="image/*"
-              onChange={(e) => setPhoto(e.target.files[0])}
+              onChange={(e) => setImage(e.target.files[0])}
               className="w-[80%] h-fit py-2 px-3 text-gray-800 bg-amber-50 rounded-[10px]"
               placeholder="Choose File"
             />
