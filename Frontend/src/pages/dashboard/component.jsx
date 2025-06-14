665f92caf2b21af617a4ab9a560e0bb666f28ec6
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-14 19:40:01 +0300

    Update component.jsx

--- Frontend/src/pages/dashboard/component.jsx
+++ Frontend/src/pages/dashboard/component.jsx
@@ -1,32 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const SideBar = () => {
+export const SideBar = ({ toggle, setToggle }) => {
   return (
-    <aside className="w-[60px] h-screen flex flex-col border-1 border-gray-700 fixed top-0 left-0 bg-transparent z-20">
-      <div className="w-full h-16 flex justify-center items-center border-b-1 border-gray-700">
+  function toggles() {
+    setToggle(!toggle);
+  }
+
+  return (
+    <aside
+      toggle={toggle}
+      setToggle={setToggle}
+      className={
+        toggle
+          ? "w-[150px] h-screen flex flex-col border-1 border-gray-700 fixed top-0 left-0 bg-white/5 backdrop-blur-sm z-30"
+          : "w-[60px] h-screen flex flex-col border-1 border-gray-700 fixed top-0 left-0 bg-white/5 backdrop-blur-sm z-30"
+      }
+    >
+      <div
+        className={
+          toggle
+            ? "w-[250px] h-16 pl-15 flex justify-between items-center border-b-1 border-gray-700"
+            : "w-[150px] h-16 pl-5 flex justify-start items-center border-b-1 border-gray-700"
+        }
+        onClick={toggles}
+      >
         <i class="fa fa-bars text-white !text-[30px]"></i>
+        {toggle && <h1 className="!text-[0px] text-transparent">DataBase</h1>}
       </div>
       <Link to="">
-        <div className="w-full h-16 flex justify-center items-center border-b-1 border-gray-700">
-          <i class="fas fa-folder-open text-white !text-[30px]"></i>
+        <div className="w-full h-16 flex justify-start items-center border-b-1 border-gray-700 space-x-2">
+          <i class="fas fa-folder-open pl-3 text-white !text-[30px]"></i>
+          {toggle && <h1 className="!text-[20px]">OverView</h1>}
         </div>
       </Link>
       <Link to="/Admin">
-        <div className="w-full h-16 flex justify-center items-center border-b-1 border-gray-700">
-          <i class="fas fa-database text-white !text-[30px]"></i>
+        <div className="w-full h-16 flex justify-start items-center border-b-1 border-gray-700 space-x-3">
+          <i class="fas fa-database pl-3 text-white !text-[30px]"></i>
+          {toggle && <h1 className="!text-[20px]">DataBase</h1>}
         </div>
       </Link>
       <Link to="/Profile">
-        <div className="w-full h-16 flex justify-center items-center border-b-1 border-gray-700">
-          <i class="fas fa-person text-white !text-[30px]"></i>
+        <div className="w-full h-16 flex justify-start items-center border-b-1 border-gray-700 space-x-5">
+          <i class="fas fa-person pl-3 text-white !text-[30px]"></i>
+          {toggle && <h1 className="!text-[20px]">Profile</h1>}
         </div>
       </Link>
     </aside>
   );
 };
 
-export const Header = () => {
+export const Header = ({ toggle }) => {
   async function handleLogout() {
     await axios.post("https://travel-x-408k.onrender.com/auth/logout", {
       withCredentials: true,
@@ -34,9 +58,13 @@ export const Header = () => {
     console.log("Logout Successfully");
   }  
   return (
-    <header className="w-screen h-16 flex justify-between pr-20 text-white text-[10px] lg:text-[20px] border-1 border-gray-700 fixed top-0 left-15 bg-white/5 backdrop-blur-sm z-20">
+    <header
+      className={`w-full h-16 flex justify-between pr-20 text-white text-[10px] lg:text-[20px] border-1 border-gray-700 fixed top-0 left-15 bg-white/5 backdrop-blur-sm z-20 ${
+        toggle ? "ml-[90px]" : "ml-0"
+      }`}
+    >
       <nav className="w-full px-5 flex justify-between items-center">
-        <div className="">
+        <div className="flex">
           <img
             src="/Logo/Time_white.png"
             className="w-15 lg:w-23 h-full p-2"
@@ -44,8 +72,12 @@ export const Header = () => {
             srcset=""
           />
         </div>
-        <div className="flex justify-between space-x-3">
-          <Link to="/Admin">
+        <div
+          className={`flex justify-center space-x-3 ${
+            toggle ? "pr-[70px]" : "ml-0"
+          }`}
+        >
+          <Link to="/SubAdmin">
             <div className="flex justify-between">Dashboard</div>
           </Link>
           <Link to="/Login">
@@ -53,9 +85,9 @@ export const Header = () => {
               Sign out
             </div>
           </Link>
+          <div className="flex items-center">Profile</div>
         </div>
       </nav>
-      <div className="flex items-center">Profile</div>
     </header>
   );
 };
