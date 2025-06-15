141ed292a1ad35b921cc829986ea2ac7e80eeb46
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-15 22:40:28 +0300

    Update BookingDb.jsx

--- Frontend/src/pages/dashboard/BookingDb.jsx
+++ Frontend/src/pages/dashboard/BookingDb.jsx
@@ -45,6 +45,7 @@ export const BookingDb = () => {
                 </div>
 
                 {data.map((user) => (
+                  <Link to="/EditBookingDb" state={{ user }}>      
                   <div className="w-full h-auto mt-8 p-5 flex flex-col lg:flex-row justify-between bg-gray-800 rounded-2xl">
                     <div className="w-full lg:w-[300px] h-auto flex flex-col items-center py-5 border-b-2 lg:border-b-0 lg:border-r-2 border-gray-600 relative">
                       <div className="relative">
@@ -106,6 +107,7 @@ export const BookingDb = () => {
                       </div>
                     </div>
                   </div>
+                  </Link>                    
                 ))}
               </div>
             </main>
