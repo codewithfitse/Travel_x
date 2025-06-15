358f4a74bc4c98d5bf27c24935f87b3d18d2caa5
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-15 22:33:33 +0300

    Update userBook.js

--- Server/models/userBook.js
+++ Server/models/userBook.js
@@ -1,14 +1,21 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  phone: String,
-  item: String,
-  destination: String,
-  message: String,
-},{ timestamps: true });
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: String,
+    lastName: String,
+    email: String,
+    phone: String,
+    destination: String,
+    item: String,
+    message: String,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
 
 const UserBook = mongoose.model("Books", UserSchema);
 export default UserBook;
