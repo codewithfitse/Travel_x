674e33000fb145b703a508947206605506f2f621
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-20 01:57:34 +0300

    Update server.js

--- Server/src/server.js
+++ Server/src/server.js
@@ -175,16 +175,6 @@ app.get("/", (req, res) => {
   console.log(`We are on Homepage`);
 });
 
-app.get("/register", (req, res) => {
-  res.json("register");
-  console.log(`We are on register`);
-});
-
-app.get("/login", (req, res) => {
-  res.json("Login");
-  console.log(`We are on Login`);
-});
-
 app.get("/dashboards", authMiddleware, async (req, res) => {
   const data = await UserLogin.find(
     { email: req.user.email },
@@ -193,38 +183,6 @@ app.get("/dashboards", authMiddleware, async (req, res) => {
   res.json(data);
   console.log(data);
 });
-
-app.put("/dashboard/:id", async (req, res) => {
-  const { firstName, lastName, email, phone, password } = req.body;
-  const Hash = await bcrypt.hash(password, 10);
-  try {
-    const updatedUser = await UserLogin.findByIdAndUpdate(
-      req.params.id,
-      { firstName, lastName, email, phone, password: Hash },
-      { new: true } // returns the updated doc
-    );
-    res.status(200).json(updatedUser);
-    console.log(updatedUser);
-  } catch (err) {
-    res.status(500).json({ error: "Failed to update user" });
-  }
-});
-
-app.delete("/dashboard/:id", async (req, res) => {
-  try {
-    const deletedUser = await UserLogin.findByIdAndDelete(req.params.id);
-
-    if (!deletedUser) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    res.status(200).json({ message: "User deleted successfully" }); 
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
 
 app.post("/register", async (req, res) => {
   const { firstName, lastName, email, phone, password } = req.body;
