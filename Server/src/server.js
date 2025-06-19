2f64555611b570218adb0383071edd032c763ce2
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-19 13:16:01 +0300

    Update server.js

--- Server/src/server.js
+++ Server/src/server.js
@@ -138,8 +138,8 @@ function isAuthenticated(req, res, next) {
 
 
 app.get("/profile", isAuthenticated, (req, res) => {
-  res.redirect("https://travel-x-kappa.vercel.app/Login");
-  //res.json({ message: "Welcome Admin", user: req.user });
+  //res.redirect("https://travel-x-kappa.vercel.app/Login");
+  res.json({ message: "Welcome Admin", user: req.user });
 });
 
 const uploadDir = 'uploads';
