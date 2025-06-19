cc3f52e16c099bf26f5d3b9d3b2d417397a83e10
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-19 17:01:31 +0300

    Update UserOauth.js

--- Server/models/UserOauth.js
+++ Server/models/UserOauth.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   avatar: String,
   isAdmin: { type: Boolean, default: false },
+  isSubAdmin: { type: Boolean, default: false },
 });
 
 export default mongoose.model("UserOauth", userSchema);
