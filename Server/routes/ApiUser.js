1e741f5e668ebe8dfa299144ffc0c2016d081f91
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-20 22:01:22 +0300

    Update ApiUser.js

--- Server/routes/ApiUser.js
+++ Server/routes/ApiUser.js
@@ -4,18 +4,21 @@ import UserOauth from "../models/UserOauth.js";
 
 const router = express.Router();
 
+// To find user of local Login and list it!
 router.get("/user", async (req, res) => {
   const data = await UserLogin.find({}).sort({ createdAt: -1 });
   res.json(data);
   console.log(`We are on Dashboard`);
 });
 
+// To find user of Google Oauth Login and list it!
 router.get("/userGoogle", async (req, res) => {
   const data = await UserOauth.find({}).sort({ createdAt: -1 });
   res.json(data);
   console.log(`We are on Dashboard`);
 });
 
+// I make this to update the local user info
 router.put("/user/:id", async (req, res) => {
   const { firstName, lastName, email, phone, password } = req.body;
   const Hash = await bcrypt.hash(password, 10);
@@ -32,9 +35,26 @@ router.put("/user/:id", async (req, res) => {
   }
 });
 
+// I make this to delete the local user info!
 router.delete("/user/:id", async (req, res) => {
   try {
     const deletedUser = await UserLogin.findByIdAndDelete(req.params.id);
+
+    if (!deletedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted successfully" }); 
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// I make this to delete the Google Oauth user info!
+router.delete("/userGoogle/:id", async (req, res) => {
+  try {
+    const deletedUser = await UserOauth.findByIdAndDelete(req.params.id);
 
     if (!deletedUser) {
       return res.status(404).json({ error: "User not found" });
