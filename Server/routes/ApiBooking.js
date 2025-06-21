405d61bf3fb12d98de19e663958c25213fc8b43a
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-21 19:50:14 +0300

    Update ApiBooking.js

--- Server/routes/ApiBooking.js
+++ Server/routes/ApiBooking.js
@@ -1,5 +1,7 @@
 import express from "express";
 import UserBook from "../models/userBook.js";
+import UserDemo from "../models/UserDemo.js";
+import authMiddleware from "../middleware/tokenMiddleware.js";
 
 const router = express.Router();
 
@@ -40,6 +42,68 @@ router.delete("/booking/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const data = await UserBook.findByIdAndDelete(id);
+    res.json(data);
+    console.log("Deleted bookings:", data);
+  } catch (err) {
+    console.error("Failed to fetch bookings:", err);
+    res
+      .status(500)
+      .json({ message: "Error fetching bookings", error: err.message });
+  }
+});
+
+
+router.get("/demo", authMiddleware, async (req, res) => {
+  const userId = req.user.id;
+  const data = await UserDemo.find({ userId }).sort({ createdAt: -1 });
+  res.json(data);
+  console.log(data);
+});
+
+router.post("/demo", authMiddleware, (req, res) => {
+  const { fullName, email, phone, item, destination } = req.body;
+  const userId = req.user.id;
+  UserDemo.create({ fullName, email, phone, item, destination, userId: userId })
+    .then((employee) => {
+      res.json(employee);
+    })
+    .catch((err) => res.json(err));
+  console.log(`Posted Successfully`);
+});
+
+router.get("/demo/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await UserDemo.find({ _id: id });
+    res.json(data);
+    console.log("Sorted Demo:", data);
+  } catch (err) {
+    console.error("Failed to fetch Demo:", err);
+    res
+      .status(500)
+      .json({ message: "Error fetching Demo", error: err.message });
+  }
+});
+
+router.patch("/demo/:id", async (req, res) => {
+  const { id } = req.params;
+  const { stat } = req.body;
+  try {
+    const data = await UserDemo.findByIdAndUpdate(id, { status: stat });
+    res.json(data);
+    console.log("Deleted bookings:", data);
+  } catch (err) {
+    console.error("Failed to fetch bookings:", err);
+    res
+      .status(500)
+      .json({ message: "Error fetching bookings", error: err.message });
+  }
+});
+
+router.delete("/demo/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await UserDemo.findByIdAndDelete(id);
     res.json(data);
     console.log("Deleted bookings:", data);
   } catch (err) {
