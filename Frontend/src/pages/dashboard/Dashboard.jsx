dd69920447c6cecb335992c0978fe80d18035c5c
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-21 21:09:21 +0300

    Update Dashboard.jsx

--- Frontend/src/pages/dashboard/Dashboard.jsx
+++ Frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
+import { Header, SideBar } from "../dashboard/component";
+import axios from "axios";
 import { Button } from "../../components/Button";
 
 const Dashboard = () => {
