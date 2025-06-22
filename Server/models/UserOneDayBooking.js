12dbaf25b1561096397f9e997d6c09fae8b78739
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-22 22:45:48 +0300

    Update UserOneDayBooking.js

--- Server/models/UserOneDayBooking.js
+++ Server/models/UserOneDayBooking.js
@@ -10,5 +10,5 @@ const ImageSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
-const UserOneDay = mongoose.model("Images", ImageSchema);
+const UserOneDay = mongoose.model("oneDayBook", ImageSchema);
 export default UserOneDay;
