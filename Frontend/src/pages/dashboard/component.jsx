d10ddc2e7479c5d276d62246e23c253e8bb97d1c
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-22 20:36:45 +0300

    Update component.jsx

--- Frontend/src/pages/dashboard/component.jsx
+++ Frontend/src/pages/dashboard/component.jsx
@@ -101,3 +101,134 @@ export const Header = ({ toggle }) => {
   );
 };
 
+
+export const SubSideBar = () => {
+  return (
+    <aside className="w-[60px] h-screen flex flex-col border-1 border-gray-700 fixed top-0 left-0 bg-transparent z-20">
+      <div className="w-full h-16 flex justify-center items-center border-b-1 border-gray-700">
+        <i class="fa fa-bars text-white !text-[30px]"></i>
+      </div>
+      <Link to="">
+        <div className="w-full h-16 flex justify-center items-center border-b-1 border-gray-700">
+          <i class="fas fa-folder-open text-white !text-[30px]"></i>
+        </div>
+      </Link>
+      <Link to="/SubAdmin">
+        <div className="w-full h-16 flex justify-center items-center border-b-1 border-gray-700">
+          <i class="fas fa-database text-white !text-[30px]"></i>
+        </div>
+      </Link>
+      <Link to="/SubProfile">
+        <div className="w-full h-16 flex justify-center items-center border-b-1 border-gray-700">
+          <i class="fas fa-person text-white !text-[30px]"></i>
+        </div>
+      </Link>
+    </aside>
+  );
+};
+
+export const SubHeader = () => {
+  //const [toggle, setToggle] = useState(false);
+  async function handleLogout() {
+    await axios.post(
+      "http://localhost:3000/logout",
+      {},
+      {
+        withCredentials: true,
+      }
+    );
+
+    console.log("Logout Successfully");
+  }  
+  return (
+    <header className="w-screen h-16 flex justify-between pr-20 text-white text-[10px] lg:text-[20px] border-1 border-gray-700 fixed top-0 left-15 bg-white/5 backdrop-blur-sm z-20">
+      <nav className="w-full px-5 flex justify-between items-center">
+        <div className="">
+          <img
+            src="/Logo/Time_white.png"
+            className="w-15 lg:w-23 h-full p-2"
+            alt=""
+            srcset=""
+          />
+        </div>
+        <div className="flex justify-between space-x-3">
+          <Link to="/">
+            <div className="flex justify-between">Dashboard</div>
+          </Link>
+          <Link to="/Login">
+            <div className="flex justify-between onClick={handleLogout}">Sign out</div>
+          </Link>
+          <Link to="/SubProfile">
+            <div className="flex justify-between">Profile</div>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export const UserSideBar = () => {
+  return (
+    <aside className="w-[60px] h-screen flex flex-col border-1 border-gray-700 fixed top-0 left-0 bg-transparent z-20">
+      <div className="w-full h-16 flex justify-center items-center border-b-1 border-gray-700">
+        <i class="fa fa-bars text-white !text-[30px]"></i>
+      </div>
+      <Link to="">
+        <div className="w-full h-16 flex justify-center items-center border-b-1 border-gray-700">
+          <i class="fas fa-folder-open text-white !text-[30px]"></i>
+        </div>
+      </Link>
+      <Link to="/Dashboard">
+        <div className="w-full h-16 flex justify-center items-center border-b-1 border-gray-700">
+          <i class="fas fa-database text-white !text-[30px]"></i>
+        </div>
+      </Link>
+      <Link to="/UserProfile">
+        <div className="w-full h-16 flex justify-center items-center border-b-1 border-gray-700">
+          <i class="fas fa-person text-white !text-[30px]"></i>
+        </div>
+      </Link>
+    </aside>
+  );
+};
+
+export const UserHeader = () => {
+  //const [toggle, setToggle] = useState(false);
+  async function handleLogout() {
+    await axios.post(
+      "http://localhost:3000/logout",
+      {},
+      {
+        withCredentials: true,
+      }
+    );
+
+    console.log("Logout Successfully");
+  }  
+  return (
+    <header className="w-screen h-16 flex justify-between pr-20 text-white text-[10px] lg:text-[20px] border-1 border-gray-700 fixed top-0 left-15 bg-white/5 backdrop-blur-sm z-20">
+      <nav className="w-full px-5 flex justify-between items-center">
+        <div className="">
+          <img
+            src="/Logo/Time_white.png"
+            className="w-15 lg:w-23 h-full p-2"
+            alt=""
+            srcset=""
+          />
+        </div>
+        <div className="flex justify-between space-x-3">
+          <Link to="/">
+            <div className="flex justify-between">Dashboard</div>
+          </Link>
+          <Link to="/Login">
+            <div className="flex justify-between onClick={handleLogout}">Sign out</div>
+          </Link>
+          <Link to="/UserProfile">
+            <div className="flex justify-between">Profile</div>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
