402f5d2a91ec328970059a4e85b2764175d63269
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-23 01:08:42 +0300

    Update DemoDb.jsx

--- Frontend/src/pages/dashboard/DemoDb.jsx
+++ Frontend/src/pages/dashboard/DemoDb.jsx
@@ -238,13 +238,13 @@ export const PendingDemoDb = () => {
                           <div className="relative">
                             <div className="w-30 h-30 flex justify-center items-center bg-amber-900 border-4 border-gray-600 rounded-full">
                               <h1 className="font-bold text-[40px]">
-                                {user.fullName?.charAt(0).toUpperCase()}{" "}
+                                {user.customName?.charAt(0).toUpperCase()}{" "}
                               </h1>
                             </div>
                             <div className="w-4 h-4 bg-green-500 rounded-full absolute bottom-0 -right-1"></div>
                           </div>
                           <h1 className="mt-2 text-2xl text-white text-center font-bold">
-                            {user.fullName}
+                            Customer:{user.customName} Owner:{user.ownerName}
                           </h1>
                           <h1 className="text-[18px] text-blue-400 text-center font-semibold">
                             {user.isAdmin === true ? "Admin" : "User"}
@@ -275,7 +275,7 @@ export const PendingDemoDb = () => {
                                 <div className="w-full h-15">
                                   <h1 className="text-gray-400">Destination</h1>
                                   <h1 className="font-semibold">
-                                    {user.destination}
+                                    {user.price}
                                   </h1>
                                 </div>
                                 <div className="w-full h-15">
@@ -375,13 +375,13 @@ export const SucessfullDemoDb = () => {
                           <div className="relative">
                             <div className="w-30 h-30 flex justify-center items-center bg-amber-900 border-4 border-gray-600 rounded-full">
                               <h1 className="font-bold text-[40px]">
-                                {user.fullName?.charAt(0).toUpperCase()}{" "}
+                                {user.customName?.charAt(0).toUpperCase()}{" "}
                               </h1>
                             </div>
                             <div className="w-4 h-4 bg-green-500 rounded-full absolute bottom-0 -right-1"></div>
                           </div>
                           <h1 className="mt-2 text-2xl text-white text-center font-bold">
-                            {user.fullName}
+                            Customer:{user.customName} Owner:{user.ownerName}
                           </h1>
                           <h1 className="text-[18px] text-blue-400 text-center font-semibold">
                             {user.isAdmin === true ? "Admin" : "User"}
@@ -412,7 +412,7 @@ export const SucessfullDemoDb = () => {
                                 <div className="w-full h-15">
                                   <h1 className="text-gray-400">Destination</h1>
                                   <h1 className="font-semibold">
-                                    {user.destination}
+                                    {user.price}
                                   </h1>
                                 </div>
                                 <div className="w-full h-15">
@@ -512,13 +512,13 @@ export const AllDemoDb = () => {
                           <div className="relative">
                             <div className="w-30 h-30 flex justify-center items-center bg-amber-900 border-4 border-gray-600 rounded-full">
                               <h1 className="font-bold text-[40px]">
-                                {user.fullName?.charAt(0).toUpperCase()}{" "}
+                                {user.customName?.charAt(0).toUpperCase()}{" "}
                               </h1>
                             </div>
                             <div className="w-4 h-4 bg-green-500 rounded-full absolute bottom-0 -right-1"></div>
                           </div>
                           <h1 className="mt-2 text-2xl text-white text-center font-bold">
-                            {user.fullName}
+                            Customer:{user.customName} Owner:{user.ownerName}
                           </h1>
                           <h1 className="text-[18px] text-blue-400 text-center font-semibold">
                             {user.isAdmin === true ? "Admin" : "User"}
@@ -549,7 +549,7 @@ export const AllDemoDb = () => {
                                 <div className="w-full h-15">
                                   <h1 className="text-gray-400">Destination</h1>
                                   <h1 className="font-semibold">
-                                    {user.destination}
+                                    {user.price}
                                   </h1>
                                 </div>
                                 <div className="w-full h-15">
@@ -649,13 +649,13 @@ export const CancledDemoDb = () => {
                           <div className="relative">
                             <div className="w-30 h-30 flex justify-center items-center bg-amber-900 border-4 border-gray-600 rounded-full">
                               <h1 className="font-bold text-[40px]">
-                                {user.fullName?.charAt(0).toUpperCase()}{" "}
+                                {user.customName?.charAt(0).toUpperCase()}{" "}
                               </h1>
                             </div>
                             <div className="w-4 h-4 bg-green-500 rounded-full absolute bottom-0 -right-1"></div>
                           </div>
                           <h1 className="mt-2 text-2xl text-white text-center font-bold">
-                            {user.fullName}
+                            Customer:{user.customName} Owner:{user.ownerName}
                           </h1>
                           <h1 className="text-[18px] text-blue-400 text-center font-semibold">
                             {user.isAdmin === true ? "Admin" : "User"}
@@ -686,7 +686,7 @@ export const CancledDemoDb = () => {
                                 <div className="w-full h-15">
                                   <h1 className="text-gray-400">Destination</h1>
                                   <h1 className="font-semibold">
-                                    {user.destination}
+                                    {user.price}
                                   </h1>
                                 </div>
                                 <div className="w-full h-15">
