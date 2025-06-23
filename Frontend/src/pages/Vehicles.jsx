e4527cfad8df183bd9cc0c8d532f9bedefce1c1c
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-23 03:11:19 +0300

    Update Vehicles.jsx

--- Frontend/src/pages/Vehicles.jsx
+++ Frontend/src/pages/Vehicles.jsx
@@ -812,8 +812,14 @@ export const OneDayVehiclesBook = () => {
                 <li>° Perfect for in city</li>
                 <li>° Pick up at airport</li>
               </ul>
-              <Link to="/Home"><h1 className="text-[30px]" onClick={handleBooking}>Okey</h1></Link>
-              <Link to="/Home"><h1 className="text-[30px]">Back</h1></Link>
+              <button className="w-fit h-fit py-2 px-4 lg:py-2 lg:px-3 text-[17px] lg:text-[30px] font-bold bg-gradient-to-r from-emerald-500 to-emerald-900 rounded-[10px] hover:shadow-emerald-500/80 hover:shadow-lg" onClick={handleBooking}>
+                {text}
+              </button>
+              <Link to="/Vehicles"><h1 className="text-[30px]">
+                <button className="w-fit h-fit py-2 px-4 lg:py-2 lg:px-3 text-[17px] lg:text-[30px] text-white font-bold bg-gradient-to-r from-red-500 to-red-900 rounded-[10px] hover:shadow-emerald-500/80 hover:shadow-lg" onClick={handleBooking}>
+                  Back
+                </button>
+              </Link>
             </div>
           </div>
         )}
