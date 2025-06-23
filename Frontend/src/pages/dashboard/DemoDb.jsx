0a4717556cefbea61d2cfddca497bbe55da5071f
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-23 20:50:02 +0300

    Update DemoDb.jsx

--- Frontend/src/pages/dashboard/DemoDb.jsx
+++ Frontend/src/pages/dashboard/DemoDb.jsx
@@ -104,7 +104,7 @@ export const LandingDemoDb = () => {
                         <h1
                           className={`font-bold transition-all duration-300 ease-in-out ${
                             toggle
-                              ? "w-fit text-[22px] sm:text-[14px] md:text-[18px] lg:text-[24px] xl:text-[30px]"
+                              ? "w-fit text-[20px] sm:text-[14px] md:text-[18px] lg:text-[24px] xl:text-[30px]"
                               : "w-full text-[28px] sm:text-[32px] md:text-[46px] lg:text-[40px] xl:text-[44px]"
                           }`}
                         >Successful</h1>
