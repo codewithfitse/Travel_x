4afb5533f6fd3f3c3b1cd897fd98ea6e6c8badcb
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-23 20:28:57 +0300

    Update DemoDb.jsx

--- Frontend/src/pages/dashboard/DemoDb.jsx
+++ Frontend/src/pages/dashboard/DemoDb.jsx
@@ -62,7 +62,7 @@ export const LandingDemoDb = () => {
                           : "text-[300px] sm:text-[360px] md:text-[400px] lg:text-[44px] xl:text-[48px]"
                       }`}
                     >
-                      <div className="w-full px-5 flex justify-between items-center">
+                      <div className="w-fit px-5 flex justify-between items-center">
                         <h1
                           className={`font-bold transition-all duration-300 ease-in-out ${
                             toggle
