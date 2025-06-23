92a4e7c1631060b93a3db35b1d75bde752d3b24b
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-23 20:24:38 +0300

    Update DemoDb.jsx

--- Frontend/src/pages/dashboard/DemoDb.jsx
+++ Frontend/src/pages/dashboard/DemoDb.jsx
@@ -64,10 +64,10 @@ export const LandingDemoDb = () => {
                     >
                       <div className="w-full px-5 flex justify-between items-center">
                         <h1
-                          className={`w-full font-bold transition-all duration-300 ease-in-out ${
+                          className={`font-bold transition-all duration-300 ease-in-out ${
                             toggle
-                              ? "w-auto text-[22px] sm:text-[14px] md:text-[18px] lg:text-[24px] xl:text-[30px]"
-                              : "text-[28px] sm:text-[32px] md:text-[46px] lg:text-[40px] xl:text-[44px]"
+                              ? "w-fit text-[22px] sm:text-[14px] md:text-[18px] lg:text-[24px] xl:text-[30px]"
+                              : "w-full text-[28px] sm:text-[32px] md:text-[46px] lg:text-[40px] xl:text-[44px]"
                           }`}
                         >All</h1>
                         <i class="fa fa-address-book w-2 !text-[30px]"></i>
