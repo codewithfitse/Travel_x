9c61ddf4f70125572d766a8dafa2f23ef9c29da9
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-27 00:50:03 +0300

    add

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -70,7 +70,7 @@ const Header = () => {
             <Link to="/Login">
               <h1 className="hover:text-[#16fe01]">LOGIN</h1>
             </Link>
-            <Link to="/Demo">
+            <Link to="/USerDashboard">
               <h1 className="hover:text-[#16fe01]">DashBoard</h1>
             </Link>
           </div>
