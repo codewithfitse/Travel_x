c69a0791cb59d360064800c91730313e2221855c
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-27 00:56:23 +0300

    Profile

--- Frontend/src/pages/dashboard/Profile.jsx
+++ Frontend/src/pages/dashboard/Profile.jsx
@@ -96,7 +96,7 @@ export const Profile = () => {
                         {user.firstName} {user.lastName}
                       </h1>
                       <h1 className="text-[18px] text-blue-400 text-center font-semibold">
-                        Admin
+                        {user?.isAdmin ? "Admin" : "SubAdmin"}
                       </h1>
                       <h1 className="text-[13px] text-gray-400 text-center font-semibold">
                         Member since January 15, 2023
