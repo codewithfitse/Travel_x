9d70f1595131a9a76282f788dbda5cd0ba3b9a53
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-26 16:48:38 +0300

    Vehicle

--- Frontend/src/pages/Vehicles.jsx
+++ Frontend/src/pages/Vehicles.jsx
@@ -116,7 +116,7 @@ const Vehicles = () => {
             <div className="pl-5">
               <h1 className="text-[40px] text-[#16fe01] font-bold">Full Suv</h1>
               <h2>Starting $60</h2>
-              <h2>Toyota VITZ or Similar</h2>
+              <h2>Jetour X70 or Similar</h2>
               <h2>Vehicle Feature</h2>
               <ul>
                 <li>° 7 to 8 Person Seat</li>
