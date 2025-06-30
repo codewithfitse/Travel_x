e8be23593373744635b1944dbf5135da102a6c40
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-30 03:23:11 +0300

    Change on vehickes

--- Frontend/src/pages/Vehicles.jsx
+++ Frontend/src/pages/Vehicles.jsx
@@ -822,7 +822,7 @@ export const OneDayVehicles = () => {
     fetchImages();
   }, []);
 
-  const handlePriceRangeClick = (g) => {
+  const handlePriceRangeClick = () => {
     navigate(`/Pricing`);
   };
 
