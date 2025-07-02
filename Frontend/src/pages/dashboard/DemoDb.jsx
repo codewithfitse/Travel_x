7fbbb40aaf9ef5d350fa5e2594b2ff22b72f147d
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-02 22:56:52 +0300

    DEmoDb

--- Frontend/src/pages/dashboard/DemoDb.jsx
+++ Frontend/src/pages/dashboard/DemoDb.jsx
@@ -97,7 +97,7 @@ export const LandingDemoDb = () => {
                     </div>
                   </Link>
 
-                  <Link to="/PendingDemoDb" state={{ status: "pending" }}>
+                  <Link to="/StatusOfBook" state={{ status: "pending" }}>
                     <div
                       className={`w-full h-auto mt-4 lg:mt-8 p-5 flex flex-col lg:flex-row justify-between bg-gray-800 rounded-2xl  ${
                         toggle
@@ -119,7 +119,7 @@ export const LandingDemoDb = () => {
                     </div>
                   </Link>
 
-                  <Link to="/PendingDemoDb" state={{ status: "successful" }}>
+                  <Link to="/StatusOfBook" state={{ status: "successful" }}>
                     <div
                       className={`w-full h-auto mt-4 lg:mt-8 p-5 flex flex-col lg:flex-row justify-between bg-gray-800 rounded-2xl  ${
                         toggle
@@ -141,7 +141,7 @@ export const LandingDemoDb = () => {
                     </div>
                   </Link>
 
-                  <Link to="/PendingDemoDb" state={{ status: "canceled" }}>
+                  <Link to="/StatusOfBook" state={{ status: "canceled" }}>
                     <div
                       className={`w-full h-auto mt-4 lg:mt-8 p-5 flex flex-col lg:flex-row justify-between bg-gray-800 rounded-2xl  ${
                         toggle
@@ -266,7 +266,7 @@ export const LandingDemoDb = () => {
   );
 };
 
-export const PendingDemoDb = () => {
+export const StatusOfBook = () => {
   const [data, setData] = useState([]);
   const [toggle, setToggle] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
@@ -413,150 +413,6 @@ export const PendingDemoDb = () => {
   );
 };
 
-export const SucessfullDemoDb = () => {
-  const [data, setData] = useState([]);
-  const [toggle, setToggle] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    async function load() {
-      try {
-        await axios
-          .get(
-            "https://travel-x-408k.onrender.com/dashboard/OneDayVehiclesBook/Successful",
-            {
-              withCredentials: true,
-            }
-          )
-          .then((result) => {
-            console.log(result.data);
-            setData(result.data);
-            setIsLoading(false);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    load();
-  }, []);
-
-  return (
-    <>
-      <section className="min-h-screen overflow-x-hidden">
-        <div className="w-full h-screen flex bg-[#020817] text-white">
-          <SideBar toggle={toggle} setToggle={setToggle} />
-          <div className="ml-14 flex flex-col flex-1">
-            <Header toggle={toggle} />
-            <main className="pt-20 p-5 bg-transparent">
-              <div
-                className={`h-full lg:px-30 bg-gray-900 transition-all duration-300 ease-in-out ${
-                  toggle ? "w-auto ml-22 p-3" : "p-5 "
-                }`}
-              >
-                <div className="w-full h-fit flex justify-between items-center">
-                  <div className="w-fit h-full py-1">
-                    <h1
-                      className={`text-white font-bold ${
-                        toggle ? "text-[20px]" : "text-[30px]"
-                      }`}
-                    >
-                      Status Successful
-                    </h1>
-                  </div>
-                  <div className="w-fit h-full">
-                    <Link to="/LandingDemoDb">
-                      <button className="px-3 py-2 text-white font-semibold rounded-[10px] bg-blue-500">
-                        Back
-                      </button>
-                    </Link>
-                  </div>
-                </div>
-
-                {isLoading && <h1>Loading...</h1>}
-                {(data &&
-                  data.map((user) => (
-                    <Link to="/EditDemoDb" state={{ user }}>
-                      <div className="w-full h-auto mt-8 p-5 flex flex-col lg:flex-row justify-between bg-gray-800 rounded-2xl">
-                        <div className="w-full lg:w-[300px] h-auto flex flex-col items-center py-5 border-b-2 lg:border-b-0 lg:border-r-2 border-gray-600 relative">
-                          <div className="relative">
-                            <div className="w-30 h-30 flex justify-center items-center bg-amber-900 border-4 border-gray-600 rounded-full">
-                              <h1 className="font-bold text-[40px]">
-                                {user.customName?.charAt(0).toUpperCase()}{" "}
-                              </h1>
-                            </div>
-                            <div className="w-4 h-4 bg-green-500 rounded-full absolute bottom-0 -right-1"></div>
-                          </div>
-                          <h1 className="mt-2 text-2xl text-white text-center font-bold">
-                            Customer:{user.customName} Car Name:{user.ownerName}
-                          </h1>
-                          <h1 className="text-[18px] text-blue-400 text-center font-semibold">
-                            Model:{user.model}
-                          </h1>
-                          <h1 className="text-[13px] text-gray-400 text-center font-semibold">
-                            {/* Member since January 15, 2023{" "} */}
-                            {new Date(user.createdAt).toLocaleString("en-GB")}
-                          </h1>
-                        </div>
-                        <div className="w-full h-full py-5 px-5 bg-gray-800">
-                          <div className="w-full h-full">
-                            <div className="border-b-2 pb-2 border-gray-400">
-                              <div className="w-full flex flex-col lg:flex-row gap-1">
-                                <div className="w-full h-15">
-                                  <h1 className="text-gray-400">Email</h1>
-                                  <h1 className="font-semibold">
-                                    {user.email}
-                                  </h1>
-                                </div>
-                                <div className="w-full h-15">
-                                  <h1 className="text-gray-400">Phone</h1>
-                                  <h1 className="font-semibold">
-                                    {user.phone}
-                                  </h1>
-                                </div>
-                              </div>
-                              <div className="w-full mt-1 flex flex-col lg:flex-row gap-1">
-                                <div className="w-full h-15">
-                                  <h1 className="text-gray-400">Price</h1>
-                                  <h1 className="font-semibold">
-                                    {user.price}
-                                  </h1>
-                                </div>
-                                <div className="w-full h-15">
-                                  <h1 className="text-gray-400">Car type</h1>
-                                  <h1 className="font-semibold">{user.item}</h1>
-                                </div>
-                              </div>
-                            </div>
-                            <div className="w-full h-fit py-2 flex flex-col space-y-3">
-                              <div className="w-fit h-full py-1">
-                                <h1 className="text-[20px] text-gray-300 font-bold">
-                                  Status
-                                </h1>
-                              </div>
-                              <div className="w-full h-fit flex gap-3">
-                                <h1
-                                  className={`capitalize ${
-                                    user.status === "Pending" && "text-green"
-                                  }`}
-                                >
-                                  {user.status}
-                                </h1>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </Link>
-                  ))) || <h1>There is now Bookings here</h1>}
-              </div>
-            </main>
-          </div>
-        </div>
-      </section>
-    </>
-  );
-};
-
 export const AllDemoDb = () => {
   const [data, setData] = useState([]);
   const [toggle, setToggle] = useState(false);
@@ -605,150 +461,6 @@ export const AllDemoDb = () => {
                       }`}
                     >
                       Status All
-                    </h1>
-                  </div>
-                  <div className="w-fit h-full">
-                    <Link to="/LandingDemoDb">
-                      <button className="px-3 py-2 text-white font-semibold rounded-[10px] bg-blue-500">
-                        Back
-                      </button>
-                    </Link>
-                  </div>
-                </div>
-
-                {isLoading && <h1>Loading...</h1>}
-                {(data &&
-                  data.map((user) => (
-                    <Link to="/EditDemoDb" state={{ user }}>
-                      <div className="w-full h-auto mt-8 p-5 flex flex-col lg:flex-row justify-between bg-gray-800 rounded-2xl">
-                        <div className="w-full lg:w-[300px] h-auto flex flex-col items-center py-5 border-b-2 lg:border-b-0 lg:border-r-2 border-gray-600 relative">
-                          <div className="relative">
-                            <div className="w-30 h-30 flex justify-center items-center bg-amber-900 border-4 border-gray-600 rounded-full">
-                              <h1 className="font-bold text-[40px]">
-                                {user.customName?.charAt(0).toUpperCase()}{" "}
-                              </h1>
-                            </div>
-                            <div className="w-4 h-4 bg-green-500 rounded-full absolute bottom-0 -right-1"></div>
-                          </div>
-                          <h1 className="mt-2 text-2xl text-white text-center font-bold">
-                            Customer:{user.customName} Car Name:{user.ownerName}
-                          </h1>
-                          <h1 className="text-[18px] text-blue-400 text-center font-semibold">
-                            Model:{user.model}
-                          </h1>
-                          <h1 className="text-[13px] text-gray-400 text-center font-semibold">
-                            {/* Member since January 15, 2023{" "} */}
-                            {new Date(user.createdAt).toLocaleString("en-GB")}
-                          </h1>
-                        </div>
-                        <div className="w-full h-full py-5 px-5 bg-gray-800">
-                          <div className="w-full h-full">
-                            <div className="border-b-2 pb-2 border-gray-400">
-                              <div className="w-full flex flex-col lg:flex-row gap-1">
-                                <div className="w-full h-15">
-                                  <h1 className="text-gray-400">Email</h1>
-                                  <h1 className="font-semibold">
-                                    {user.email}
-                                  </h1>
-                                </div>
-                                <div className="w-full h-15">
-                                  <h1 className="text-gray-400">Phone</h1>
-                                  <h1 className="font-semibold">
-                                    {user.phone}
-                                  </h1>
-                                </div>
-                              </div>
-                              <div className="w-full mt-1 flex flex-col lg:flex-row gap-1">
-                                <div className="w-full h-15">
-                                  <h1 className="text-gray-400">Price</h1>
-                                  <h1 className="font-semibold">
-                                    {user.price}
-                                  </h1>
-                                </div>
-                                <div className="w-full h-15">
-                                  <h1 className="text-gray-400">Car type</h1>
-                                  <h1 className="font-semibold">{user.item}</h1>
-                                </div>
-                              </div>
-                            </div>
-                            <div className="w-full h-fit py-2 flex flex-col space-y-3">
-                              <div className="w-fit h-full py-1">
-                                <h1 className="text-[20px] text-gray-300 font-bold">
-                                  Status
-                                </h1>
-                              </div>
-                              <div className="w-full h-fit flex gap-3">
-                                <h1
-                                  className={`capitalize ${
-                                    user.status === "Pending" && "text-green"
-                                  }`}
-                                >
-                                  {user.status}
-                                </h1>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </Link>
-                  ))) || <h1>There is now Bookings here</h1>}
-              </div>
-            </main>
-          </div>
-        </div>
-      </section>
-    </>
-  );
-};
-
-export const CancledDemoDb = () => {
-  const [data, setData] = useState([]);
-  const [toggle, setToggle] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    async function load() {
-      try {
-        await axios
-          .get(
-            "https://travel-x-408k.onrender.com/dashboard/OneDayVehiclesBook/Canceled",
-            {
-              withCredentials: true,
-            }
-          )
-          .then((result) => {
-            console.log(result.data);
-            setData(result.data);
-            setIsLoading(false);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    load();
-  }, []);
-
-  return (
-    <>
-      <section className="min-h-screen overflow-x-hidden">
-        <div className="w-full h-screen flex bg-[#020817] text-white">
-          <SideBar toggle={toggle} setToggle={setToggle} />
-          <div className="ml-14 flex flex-col flex-1">
-            <Header toggle={toggle} />
-            <main className="pt-20 p-5 bg-transparent">
-              <div
-                className={`h-full lg:px-30 bg-gray-900 transition-all duration-300 ease-in-out ${
-                  toggle ? "w-auto ml-22 p-3" : "p-5 "
-                }`}
-              >
-                <div className="w-full h-fit flex justify-between items-center">
-                  <div className="w-fit h-full py-1">
-                    <h1
-                      className={`text-white font-bold ${
-                        toggle ? "text-[20px]" : "text-[30px]"
-                      }`}
-                    >
-                      Status Cancel
                     </h1>
                   </div>
                   <div className="w-fit h-full">
