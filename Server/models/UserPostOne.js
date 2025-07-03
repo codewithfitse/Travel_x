3ef562c38ecbe736b29c7c1344168a0aa8ba1e34
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-03 12:55:42 +0300

    UPDATED SERVER MAKE EDITED ON ONDAYPOST SCHEMA

--- Server/models/UserPostOne.js
+++ Server/models/UserPostOne.js
@@ -1,36 +1,37 @@
 import mongoose from "mongoose";
 
-const ImageSchema = new mongoose.Schema({
-  userId: String,
-  public_id: String,
-  filename: {
-    type: String,
+const ImageSchema = new mongoose.Schema(
+  {
+    userId: String,
+    public_id: String,
+    filename: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    item: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    model: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+    },
   },
-  url: {
-    type: String,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  item: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  model: {
-    type: String,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    default: 1,
-  },
-  createdAt: { type: Date, default: Date.now },
-});
+  { timestamps: true }
+);
 
 const UserPostOne = mongoose.model("oneImages", ImageSchema);
 export default UserPostOne;
