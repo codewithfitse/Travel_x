f7c6485b9f51d395fc1cc688bf56079213abb4a5
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-03 21:53:31 +0300

    LANDING PAGE

---
+++ Frontend/src/components/LiveChat.js
@@ -0,0 +1,24 @@
+// LiveChat.js
+import { useEffect } from "react";
+
+const LiveChat = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.id = "3cxchat";
+    script.src = "https://1215.3cx.cloud/time"; // your real chat.js link
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+
+    return () => {
+      const oldScript = document.getElementById("3cxchat");
+      if (oldScript) {
+        oldScript.remove();
+      }
+    };
+  }, []);
+
+  return null; // no visible UI needed
+};
+
+export default LiveChat;
