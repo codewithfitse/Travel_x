c8a739807019110641be6fcdab07db190ef3db3f
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-03 21:48:31 +0300

    LANDING PAGE

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Landing } from "./pages/Landing";
-import LiveChat from "./components/LiveChat ";
+import LiveChat from "./components/LiveChat";
 
 const App = () => {
   return (
