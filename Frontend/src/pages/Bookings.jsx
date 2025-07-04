4ad17024bc051a107f6061a0653f4e3ab567848a
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-05 00:38:31 +0300

    UPDATED THE BOOKINGS ADDED ERROR HANDLING

--- Frontend/src/pages/Bookings.jsx
+++ Frontend/src/pages/Bookings.jsx
@@ -171,7 +171,7 @@ const OneDayBook = () => {
 
             <div className="mt-10 flex justify-between space-x-2">
               {err && (
-                <h1 className="text-red-500 text-[30px] font-bold text-center">
+                <h1 className="text-red-500 text-[30px] font-bold text-center ">
                   {err}
                 </h1>
               )}
