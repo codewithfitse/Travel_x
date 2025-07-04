6395042643cfe33d6c9a30d0a463d7142852c310
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-04 15:31:33 +0300

    CHANGE ON CRUDONE ADD ERROR HANDLING on EditOne

--- Frontend/src/pages/dashboard/CRUDONE.jsx
+++ Frontend/src/pages/dashboard/CRUDONE.jsx
@@ -940,8 +940,8 @@ export const OneEdits = () => {
           navigate("/OneLandingVehicle");
         })
         .catch((err) => {
-          alert(errorMsg);
           const errorMsg = err.response?.data?.error || "Something went wrong";
+          alert(errorMsg);
           setErr(errorMsg);
           console.log(err);
         });
