d7460eba3759094ce2c1a09b07a169e87e750b6d
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-05 17:36:08 +0300

    ADD ROUTE TO MAIN AND HEADER TO TRY POPOUT

--- Frontend/src/pages/dashboard/Demo.jsx
+++ Frontend/src/pages/dashboard/Demo.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
 export const Demo = () => {
+  const btn =
+    "flex gap-2 items-center justify-center text-[30px] font-bold py-2 px-4 rounded-lg";
+  const btnLight = "bg-white text-gray-400";
+  const btnDanger = "text-white bg-red-600 shadow-red-400/40";
+
   return (
-    <div className="w-full h-screen flex bg-gray-500">
-      <div>Demo</div>
+    <div className="w-full h-screen flex items-center justify-center bg-gray-500">
+      <div>
+        <button className={`${btn} ${btnDanger}`}>Delete</button>
+      </div>
     </div>
   );
 };
