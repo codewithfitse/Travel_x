3ac09946e93fc5d3e906dc582b818d8b5d936b1b
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-06 17:47:57 +0300

    UPDATED THE BACKEND AND USERONEDAYBOOKING MODEL TO ADD NESTED OBJECT AND MAKE CHANGE ON THE APIBOOKING ROUTES ON POST TO ADD ERROR HANDLING TO CATCH DUPILICATED TRANSATION

--- Server/routes/ApiBooking.js
+++ Server/routes/ApiBooking.js
@@ -56,7 +56,7 @@ router.post("/OneDayVehiclesBook", authMiddleware, async (req, res) => {
   try {
     const Transaction = await UserOneDay.find({ transactionId });
 
-    if (Transaction) {
+    if (Transaction === transactionId) {
       return res
         .status(403)
         .json({ error: "Something Wrong you must Been booked before" });
