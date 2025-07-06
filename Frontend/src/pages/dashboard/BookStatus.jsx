339e2d2162ed8b08efb0f332ac6a50dfc0f6a1f7
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-07 00:32:17 +0300

    UPDATED THE BOOKSTATUS

--- Frontend/src/pages/dashboard/BookStatus.jsx
+++ Frontend/src/pages/dashboard/BookStatus.jsx
@@ -1064,9 +1064,17 @@ export const EditBookStatus = () => {
                   <div className="w-full lg:w-[300px] h-auto flex flex-col items-center py-5 border-b-2 lg:border-b-0 lg:border-r-2 border-gray-600 relative">
                     <div className="relative">
                       <div className="w-30 h-30 flex justify-center items-center bg-amber-900 border-4 border-gray-600 rounded-full">
-                        <h1 className="font-bold text-[40px]">
-                          {user?.data?.customName?.charAt(0).toUpperCase()}{" "}
-                        </h1>
+                        {user.data.avatar ? (
+                          <img
+                            src={user?.data?.avatar}
+                            className="w-full h-full object-cover rounded-full"
+                            alt="Profile Pic"
+                          />
+                        ) : (
+                          <h1 className="font-bold text-[40px]">
+                            {user?.data?.customName?.charAt(0).toUpperCase()}{" "}
+                          </h1>
+                        )}
                       </div>
                       <div className="w-4 h-4 bg-green-500 rounded-full absolute bottom-0 -right-1"></div>
                     </div>
