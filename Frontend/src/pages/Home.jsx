f0dc9f74c8b3e47e1007915da35593d1e212687a
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-08 00:14:56 +0300

    UPDATE THE HOME TO USE WEBP

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -96,7 +96,7 @@ const Home = () => {
               isLoading={isLoading}
               src="compact.png"
               imgClass="w-full mt-13 lg:mt-5 lg:w-[400px] h-fit"
-              skeletonClass="w-[400px] h-[300px] bg-gray-400  rounded-[10px] animate-pulse"
+              skeletonClass="w-[400px] lg:w-[300px] h-[300px] bg-gray-400  rounded-[10px] animate-pulse"
             />
             <h1 className="text-[50px] text-white font-bold">Compact</h1>
             <p className="text-[25px] text-white ">From $1560 a Day</p>
@@ -111,7 +111,7 @@ const Home = () => {
               isLoading={isLoading}
               src="economy.png"
               imgClass="w-full mt-13 lg:mt-5 lg:w-[400px] h-fit"
-              skeletonClass="w-[400px] h-[300px] bg-gray-400  rounded-[10px] animate-pulse"
+              skeletonClass="w-[400px] lg:w-[300px] h-[300px] bg-gray-400  rounded-[10px] animate-pulse"
             />
             <h1 className="text-[50px] text-white font-bold">Economy</h1>
             <p className="text-[25px] text-white ">From $2070 a Day</p>
@@ -126,7 +126,7 @@ const Home = () => {
               isLoading={isLoading}
               src="midsuv.png"
               imgClass="w-full mt-13 lg:mt-5 lg:w-[400px] h-fit"
-              skeletonClass="w-[400px] h-[300px] bg-gray-400  rounded-[10px] animate-pulse"
+              skeletonClass="w-[400px] lg:w-[300px] h-[300px] bg-gray-400  rounded-[10px] animate-pulse"
             />
             <h1 className="text-[50px] text-white font-bold">Mid Suv</h1>
             <p className="text-[25px] text-white ">From $1770 a Day</p>
@@ -141,7 +141,7 @@ const Home = () => {
               isLoading={isLoading}
               src="minivan.png"
               imgClass="w-full mt-13 lg:mt-5 lg:w-[400px] h-fit"
-              skeletonClass="w-[400px] h-[300px] bg-gray-400  rounded-[10px] animate-pulse"
+              skeletonClass="w-[400px] lg:w-[300px] h-[300px] bg-gray-400  rounded-[10px] animate-pulse"
             />
             <h1 className="text-[50px] text-white font-bold">Mini Van</h1>
             <p className="text-[25px] text-white ">From $2570 a Day</p>
