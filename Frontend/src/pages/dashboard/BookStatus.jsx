cec764bf8ee50afb9027bdbf97a07e05910acb02
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-07 11:19:11 +0300

    UPDATED THE BOOKINGSTATUS ON EDITING

--- Frontend/src/pages/dashboard/BookStatus.jsx
+++ Frontend/src/pages/dashboard/BookStatus.jsx
@@ -1070,7 +1070,7 @@ export const EditBookStatus = () => {
   return (
     <>
       <section className="min-h-screen overflow-x-hidden">
-        <div className="w-full h-screen flex bg-[#020817] text-white">
+        <div className="w-full h-full flex bg-[#020817] text-white">
           <SideBar toggle={toggle} setToggle={setToggle} />
           <div className="ml-14 flex flex-col flex-1">
             <Header toggle={toggle} />
