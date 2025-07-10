13c92482e35b540039027cf346059f8d1608c401
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-10 20:32:49 +0300

    ADDING ENV TOO VEHICLE PART

--- Frontend/src/pages/Vehicles.jsx
+++ Frontend/src/pages/Vehicles.jsx
@@ -6,6 +6,7 @@ import { Button } from "../components/Button";
 import axios from "axios";
 import SkeletonImage from "../components/Skeleton";
 import LiveChat from "../components/LiveChat";
+const Backend = import.meta.env.VITE_BACKEND_URL;
 
 const Vehicles = () => {
   const [isLoading, setIsLoading] = useState(true);
