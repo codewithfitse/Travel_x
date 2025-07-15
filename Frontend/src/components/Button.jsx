d5b193cacdc0af1804e5d22cf49979054b495fc6
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-15 23:45:38 +0300

    UPDATE MAKE TO USE FLOWTAILWIND

--- Frontend/src/components/Button.jsx
+++ Frontend/src/components/Button.jsx
@@ -14,3 +14,15 @@ const Buttons = ({ text, color }) => {
 };
 
 export default Buttons;
+
+export const LoadingButtons = ({ text, color }) => {
+  return (
+    <Button
+      color={color}
+      outline
+      className="w-full h-fit py-2 px-4 lg:py-2 lg:px-3 text-[17px] lg:text-[30px] font-bold"
+    >
+      {text}
+    </Button>
+  );
+};
